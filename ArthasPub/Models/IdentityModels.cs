0bbb2e1e741b7005d66614bae5297fca1cea8de0
ArthasNg <keeperboyaa@HOtmail.com>
2020-02-06 22:37:58 +0800

    commit

--- ArthasPub/Models/IdentityModels.cs
+++ ArthasPub/Models/IdentityModels.cs
@@ -9,7 +9,6 @@ namespace ArthasPub.Models
     // You can add profile data for the user by adding more properties to your ApplicationUser class, please visit https://go.microsoft.com/fwlink/?LinkID=317594 to learn more.
     public class ApplicationUser : IdentityUser
     {
-        public string Something { get; set; }
         public async Task<ClaimsIdentity> GenerateUserIdentityAsync(UserManager<ApplicationUser> manager)
         {
             // Note the authenticationType must match the one defined in CookieAuthenticationOptions.AuthenticationType
@@ -22,7 +21,7 @@ namespace ArthasPub.Models
     public class ApplicationDbContext : IdentityDbContext<ApplicationUser>
     {
         public ApplicationDbContext() 
-            : base("ArthasPubUser", throwIfV1Schema: false)
+            : base("ArthasPubDB", throwIfV1Schema: false)
         {
         }
 
--- ArthasPub/Models/Order.cs
+++ ArthasPub/Models/Order.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Web;
 
@@ -7,11 +9,16 @@ namespace ArthasPub.Models
 {
     public class Order
     {
+        [Key]
         public int OrderId { get; set; }
-        public int UserId { get; set; }
+        [StringLength(128)]
+        public int UserId { get; set; } 
+        public int CartId { get; set; }
         public decimal Total { get; set; }
         public DateTime CreateDate { get; set; }
 
+        [ForeignKey("UserId")]
+        public virtual ApplicationUser User { get; set; }
         public virtual List<CartItem> CartItem { get; set; }
     }
 
